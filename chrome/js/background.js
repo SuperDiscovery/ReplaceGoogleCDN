1049313621b0e3cdadd69d1cb601cb15506e5d7f
SuperDiscovery <49646863+SuperDiscovery@users.noreply.github.com>
2019-04-15 21:47:03 +0800

    Update background.js
    use cdnjs.cloudflare.com instead

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -1,13 +1,13 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(request) {
-        var url = request.url.replace('googleapis.com', 'lug.ustc.edu.cn');
-        url = url.replace('themes.googleusercontent.com', 'google-themes.lug.ustc.edu.cn');
+        var url = request.url.replace('ajax.googleapis.com', 'cdnjs.cloudflare.com');
+        //url = url.replace('themes.googleusercontent.com', 'google-themes.lug.ustc.edu.cn');
         return {redirectUrl: url};
     },
     {
         urls: [
-            "*://ajax.googleapis.com/*",
-            "*://themes.googleusercontent.com/*"
+            "*://ajax.googleapis.com/*"//,
+            //"*://themes.googleusercontent.com/*"
         ]
     },
     ["blocking"]
