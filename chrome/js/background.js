8392a8cea6467d7dd746df3d71525c99d2263f50
SuperDiscovery <49646863+SuperDiscovery@users.noreply.github.com>
2019-06-02 00:57:49 +0800

    替换recaptcha

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -1,12 +1,14 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(request) {
         var url = request.url.replace('ajax.googleapis.com', 'cdnjs.cloudflare.com');
-        //url = url.replace('themes.googleusercontent.com', 'google-themes.lug.ustc.edu.cn');
+		url = url.replace('www.google.com/recaptcha/api.js', 'recaptcha.net/recaptcha/api.js');
+       //url = url.replace('themes.googleusercontent.com', 'google-themes.lug.ustc.edu.cn');
         return {redirectUrl: url};
     },
     {
         urls: [
-            "*://ajax.googleapis.com/*"//,
+            "*://ajax.googleapis.com/*",
+			"*://www.google.com/recaptcha/api.js"
             //"*://themes.googleusercontent.com/*"
         ]
     },
