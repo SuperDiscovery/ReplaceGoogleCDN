a2e732c7132ae74b38bda3ee77f8cc28e4fde32b
SuperDiscovery <49646863+SuperDiscovery@users.noreply.github.com>
2019-11-24 13:18:35 +0800

    Add files via upload

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -1,17 +1,22 @@
 chrome.webRequest.onBeforeRequest.addListener(
-    function(request) {
-        var url = request.url.replace('ajax.googleapis.com', 'cdnjs.cloudflare.com');
+    function (request) {
+        var url = request.url.replace('ajax.googleapis.com/ajax/libs/jquery', 'cdnjs.cloudflare.com/ajax/libs/jquery');
+        // var url = request.url.replace('ajax.googleapis.com/ajax/libs/jquery/', 'raw.githubusercontent.com/cdnjs/cdnjs/master/ajax/libs/jquery/');
+        // url = url.replace('ajax.googleapis.com/ajax/libs/jqueryui/', 'code.jquery.com/ui/');
         url = url.replace('ajax.aspnetcdn.com/ajax/jQuery/', 'code.jquery.com/');
-		url = url.replace('www.google.com/recaptcha/api.js', 'recaptcha.net/recaptcha/api.js');
-       //url = url.replace('themes.googleusercontent.com', 'google-themes.lug.ustc.edu.cn');
-        return {redirectUrl: url};
+        // url = url.replace('ajax.aspnetcdn.com/ajax/jquery.ui/', 'code.jquery.com/ui/');
+
+        url = url.replace('www.google.com/recaptcha/api.js', 'recaptcha.net/recaptcha/api.js');
+        url = url.replace('maps.googleapis.com', 'maps.google.cn');
+        return { redirectUrl: url };
     },
     {
         urls: [
             "*://ajax.googleapis.com/*",
+            "*://maps.googleapis.com/*",
             "*://ajax.aspnetcdn.com/*",
-			"*://www.google.com/recaptcha/api.js*"
-            //"*://themes.googleusercontent.com/*"
+
+            "*://www.google.com/recaptcha/api.js*"
         ]
     },
     ["blocking"]
