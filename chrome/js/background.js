a28a008e027ca1b34b2fc0c8cc20d0789bb7c5af
SuperDiscovery <49646863+SuperDiscovery@users.noreply.github.com>
2019-11-24 12:07:13 +0800

    Update background.js

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -1,6 +1,7 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(request) {
         var url = request.url.replace('ajax.googleapis.com', 'cdnjs.cloudflare.com');
+        url = url.replace('ajax.aspnetcdn.com/ajax/jQuery/', 'code.jquery.com/');
 		url = url.replace('www.google.com/recaptcha/api.js', 'recaptcha.net/recaptcha/api.js');
        //url = url.replace('themes.googleusercontent.com', 'google-themes.lug.ustc.edu.cn');
         return {redirectUrl: url};
@@ -8,6 +9,7 @@ chrome.webRequest.onBeforeRequest.addListener(
     {
         urls: [
             "*://ajax.googleapis.com/*",
+            "*://ajax.aspnetcdn.com/*",
 			"*://www.google.com/recaptcha/api.js*"
             //"*://themes.googleusercontent.com/*"
         ]
